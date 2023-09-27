6f84ff83943f34187895c4e8914252656eb741d0
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-09-27 16:48:15 -0300

    modifying contact form with title and necessary inputs

--- src/views/ContactForm.jsx
+++ src/views/ContactForm.jsx
@@ -1,7 +1,19 @@
 const ContactForm = () => {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            <article>
+                <h1>How can we help you?</h1>
+            </article>
+            <form action="">
+                <p>Email</p>
+                <input type="email" placeholder="Enter your mail here" />
+                <p>Description</p>
+                <input
+                    type="text"
+                    placeholder="Tell us more of what cake you want"
+                />
+            </form>
+        </>
+    );
 };
 export default ContactForm;
